1ba1d28a701a21e99820aa787bb498441449f399
alhawking <alfredo.ledesma0605@gmail.com>
2021-05-29 10:43:58 -0500

    Maquteación con estilos a la página de registro

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ app.use('/public', express.static(__dirname + '/public'));
 app.use('/views', express.static(__dirname + '/views'));
 app.listen(3000);
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'views/index.html')));
-app.get('/signup.html', (req, res) => res.sendFile(path.resolve(__dirname, 'views/signup.html')));
+app.get('/signup', (req, res) => res.sendFile(path.resolve(__dirname, 'views/signup.html')));
