0f3b8200d600306eda85577ae57a5cf2a63671ef
alhawking <alfredo.ledesma0605@gmail.com>
2021-05-28 21:53:56 -0500

    Se añadió ruta en express para register.html

--- app.js
+++ app.js
@@ -2,5 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use('/public', express.static(__dirname + '/public'));
+app.use('/views', express.static(__dirname + '/views'));
 app.listen(3000);
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'views/index.html')));
+app.get('/signup.html', (req, res) => res.sendFile(path.resolve(__dirname, 'views/signup.html')));
