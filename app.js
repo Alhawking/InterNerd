e0f7de4db0008be617cc6bc05101e77880b98bd5
alhawking <alfredo.ledesma0605@gmail.com>
2021-05-29 16:49:15 -0500

    Se añadÃió la ruta a la página de login

--- app.js
+++ app.js
@@ -6,3 +6,4 @@ app.use('/views', express.static(__dirname + '/views'));
 app.listen(3000);
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'views/index.html')));
 app.get('/signup', (req, res) => res.sendFile(path.resolve(__dirname, 'views/signup.html')));
+app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, 'views/login.html')));
