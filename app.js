0950338289e1474dc31f10445401ffc94cb4cc76
alhawking <alfredo.ledesma0605@gmail.com>
2021-06-15 14:38:20 -0500

    Cambio de orden de líneas de expresiones

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const path = require('path');
-const main = require('./src/controllers/main');
 const app = express();
 const mainRoute = require('./src/routes/main');
 app.use('/public', express.static(__dirname + '/public'));
