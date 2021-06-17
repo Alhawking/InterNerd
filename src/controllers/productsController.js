f525d02da37e4cfe72efaaea09b433bbaff0015d
alhawking <alfredo.ledesma0605@gmail.com>
2021-06-17 16:49:48 -0500

    Dinamismo al llenar opciones a partir de un array

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -3,6 +3,30 @@ const productsController = {
         res.render('products/detail', {
             pageTitle: 'Detalle'
         });
+    },
+    create: (req, res) => {
+        const redes = [
+                'Marvel',
+                'DC',
+                'Harry Potter',
+                'Star Wars',
+                'Simpsons'
+            ],
+            categorias = [
+                'Libros',
+                'Cómics',
+                'Escritorio',
+                'Ropa',
+                'Coleccionable'
+            ]
+        res.render('products/create', {
+            pageTitle: 'Crear',
+            redes,
+            categorias
+        });
+    },
+    edit: (req, res) => {
+        res.render('products/edit', { pageTitle: 'Editar' });
     }
 }
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -2,5 +2,7 @@ const express = require('express');
 const router = express.Router();
 const productsController = require('../controllers/productsController');
 router.get('/detail/:id?', productsController.detail);
+router.get('/create', productsController.create);
+router.get('/edit', productsController.edit);
 
 module.exports = router;
