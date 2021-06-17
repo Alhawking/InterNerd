76fd337da4105cbfb7d34f5fa6dbc936ca243283
alhawking <alfredo.ledesma0605@gmail.com>
2021-06-16 23:33:32 -0500

    Se añadió variable para designar el title de cada página

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,13 +1,19 @@
 const path = require('path');
 const main = {
     index: (req, res) => {
-        res.render('index');
+        res.render('index', {
+            pageTitle: 'Home'
+        });
     },
     login: (req, res) => {
-        res.render('login');
+        res.render('login', {
+            pageTitle: 'Login'
+        });
     },
     register: (req, res) => {
-        res.render('signup');
+        res.render('signup', {
+            pageTitle: 'Signup'
+        });
     }
 }
 
