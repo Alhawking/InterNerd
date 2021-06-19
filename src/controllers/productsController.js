bcf498e703f8947082a4c4494c3a07900fe9ed13
alhawking <alfredo.ledesma0605@gmail.com>
2021-06-19 09:59:00 -0500

    Se añadieron productos temporales de prueba

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,3 +1,43 @@
+function Product(name, description, stock, price, especifications, image, red, category) {
+    this.name = name;
+    this.description = description;
+    this.stock = stock;
+    this.price = price;
+    this.especifications = especifications
+    this.image = image;
+    this.category = category;
+    this.red = red;
+}
+const temp = {
+    name: 'Funko Harry Potter con uniforme',
+    description: 'Funko de Harry Potter con uniforme de Hogwarts',
+    stock: true,
+    price: 300.00,
+    especifications: {},
+    image: '',
+    red: 'hp',
+    category: 'Coleccionable'
+}
+
+const products = [
+    new Product('Funko Harry Potter con uniforme', 'Funko de Harry Potter con uniforme de Hogwarts', true, 300.00, { Altura: 10.16, Materiales: 'PVC', peso: 0.13 }, '', 'hp', 'Coleccionable'),
+    new Product('Funko Harry Potter con uniforme', 'Funko de Harry Potter con uniforme de Hogwarts', true, 300.00, { Altura: 10.16, Materiales: 'PVC', peso: 0.13 }, '', 'hp', 'Coleccionable'),
+    new Product('Funko Harry Potter con uniforme', 'Funko de Harry Potter con uniforme de Hogwarts', true, 300.00, { Altura: 10.16, Materiales: 'PVC', peso: 0.13 }, '', 'hp', 'Coleccionable'),
+    new Product('Funko Harry Potter con uniforme', 'Funko de Harry Potter con uniforme de Hogwarts', true, 300.00, { Altura: 10.16, Materiales: 'PVC', peso: 0.13 }, '', 'hp', 'Coleccionable'),
+    new Product('Funko Harry Potter con uniforme', 'Funko de Harry Potter con uniforme de Hogwarts', true, 300.00, { Altura: 10.16, Materiales: 'PVC', peso: 0.13 }, '', 'hp', 'Coleccionable'),
+    new Product('Funko Harry Potter con uniforme', 'Funko de Harry Potter con uniforme de Hogwarts', true, 300.00, { Altura: 10.16, Materiales: 'PVC', peso: 0.13 }, '', 'hp', 'Coleccionable'),
+    new Product('Funko Harry Potter con uniforme', 'Funko de Harry Potter con uniforme de Hogwarts', true, 300.00, { Altura: 10.16, Materiales: 'PVC', peso: 0.13 }, '', 'hp', 'Coleccionable'),
+    new Product('Funko Harry Potter con uniforme', 'Funko de Harry Potter con uniforme de Hogwarts', true, 300.00, { Altura: 10.16, Materiales: 'PVC', peso: 0.13 }, '', 'hp', 'Coleccionable'),
+    new Product('Funko Harry Potter con uniforme', 'Funko de Harry Potter con uniforme de Hogwarts', true, 300.00, { Altura: 10.16, Materiales: 'PVC', peso: 0.13 }, '', 'hp', 'Coleccionable'),
+    new Product('Funko Harry Potter con uniforme', 'Funko de Harry Potter con uniforme de Hogwarts', true, 300.00, { Altura: 10.16, Materiales: 'PVC', peso: 0.13 }, '', 'hp', 'Coleccionable'),
+    new Product('Funko Harry Potter con uniforme', 'Funko de Harry Potter con uniforme de Hogwarts', true, 300.00, { Altura: 10.16, Materiales: 'PVC', peso: 0.13 }, '', 'hp', 'Coleccionable'),
+    new Product('Funko Harry Potter con uniforme', 'Funko de Harry Potter con uniforme de Hogwarts', true, 300.00, { Altura: 10.16, Materiales: 'PVC', peso: 0.13 }, '', 'hp', 'Coleccionable'),
+    new Product('Funko Harry Potter con uniforme', 'Funko de Harry Potter con uniforme de Hogwarts', true, 300.00, { Altura: 10.16, Materiales: 'PVC', peso: 0.13 }, '', 'hp', 'Coleccionable'),
+    new Product('Funko Harry Potter con uniforme', 'Funko de Harry Potter con uniforme de Hogwarts', true, 300.00, { Altura: 10.16, Materiales: 'PVC', peso: 0.13 }, '', 'hp', 'Coleccionable'),
+    new Product('Funko Harry Potter con uniforme', 'Funko de Harry Potter con uniforme de Hogwarts', true, 300.00, { Altura: 10.16, Materiales: 'PVC', peso: 0.13 }, '', 'hp', 'Coleccionable'),
+    new Product('Funko Harry Potter con uniforme', 'Funko de Harry Potter con uniforme de Hogwarts', true, 300.00, { Altura: 10.16, Materiales: 'PVC', peso: 0.13 }, '', 'hp', 'Coleccionable'),
+    new Product('Funko Harry Potter con uniforme', 'Funko de Harry Potter con uniforme de Hogwarts', true, 300.00, { Altura: 10.16, Materiales: 'PVC', peso: 0.13 }, '', 'hp', 'Coleccionable')
+]
 const productsController = {
     detail: (req, res) => {
         res.render('products/detail', {
