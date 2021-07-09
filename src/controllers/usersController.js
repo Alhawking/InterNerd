9de055d3aa2789c141adbbc637eae39657b4061d
francisco espinola <franespinola_92@hotmail.com>
2021-07-09 17:46:41 -0300

    agregue validacion formulario

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,3 +1,13 @@
+//requiriendo express validator//
+const{validationResult}=require('express-validator');
+const fs = require('fs');
+const path = require('path');
+
+const usersFilePath = path.join(__dirname, '../data/db.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+//----------------------------//
 const usersController = {
     login: (req, res) => {
         res.render('users/login', {
@@ -14,7 +24,24 @@ const usersController = {
             pageTitle: 'Perfil',
             id: req.params.id
         })
+    },
+    edit:(req,res)=>{
+      
+        res.render('users/editUser',{
+            pageTitle:'Editar'
+        })
+        
+    },
+    editPut:(req,res)=>{
+        const resultValidation=validationResult(req)
+        if(resultValidation.errors.length>0){
+            return res.render('users/editUser',{
+                errors:resultValidation.mapped(),
+                pageTitle:'Editar',
+                oldData:req.body
+            })
+    
+        }
     }
 }
-
 module.exports = usersController;
