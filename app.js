0652754f17ac7948d51962123f731c20b3e1a5ae
francisco espinola <franespinola_92@hotmail.com>
2021-07-09 17:43:51 -0300

    correccion methodOverride

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const app = express();
 const mainRoutes = require('./src/routes/main');
 const productsRoutes = require('./src/routes/products');
 const usersRoutes = require('./src/routes/users')
+const methodOverride=require('method-override');
 app.use('/public', express.static(__dirname + '/public'));
+app.use(express.urlencoded({extended:false}));
+app.use(methodOverride('_method'));
+app.use(methodOverride('_METHOD'));
 app.use('', mainRoutes);
 app.use('/users', usersRoutes);
 app.use('/products', productsRoutes);
@@ -12,10 +16,6 @@ app.set('view engine', 'ejs');
 
 
 //trabajando con POST//
-app.use(express.urlencoded({extended:false}));
+
 app.use(express.json());
 
-//configurando para poder usar PUT y delete//
-
-const methodOverride=require('method-override');
-app.use(methodOverride('_method'));
