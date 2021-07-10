4026456830e2a05edf2a30b2cb75ada8d8029393
francisco espinola <franespinola_92@hotmail.com>
2021-07-09 21:16:44 -0300

    di formato a documento

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,47 +1,44 @@
 //requiriendo express validator//
-const{validationResult}=require('express-validator');
-const fs = require('fs');
-const path = require('path');
+const { validationResult } = require("express-validator");
+const fs = require("fs");
+const path = require("path");
 
-const usersFilePath = path.join(__dirname, '../data/db.json');
-const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+const usersFilePath = path.join(__dirname, "../data/users.json");
+const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
 
-const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 //----------------------------//
 const usersController = {
-    login: (req, res) => {
-        res.render('users/login', {
-            pageTitle: 'Login'
-        })
-    },
-    signup: (req, res) => {
-        res.render('users/signup', {
-            pageTitle: 'Registrarse'
-        })
-    },
-    profile: (req, res) => {
-        res.render('users/profile', {
-            pageTitle: 'Perfil',
-            id: req.params.id
-        })
-    },
-    edit:(req,res)=>{
-      
-        res.render('users/editUser',{
-            pageTitle:'Editar'
-        })
-        
-    },
-    editPut:(req,res)=>{
-        const resultValidation=validationResult(req)
-        if(resultValidation.errors.length>0){
-            return res.render('users/editUser',{
-                errors:resultValidation.mapped(),
-                pageTitle:'Editar',
-                oldData:req.body
-            })
-    
-        }
+  login: (req, res) => {
+    res.render("users/login", {
+      pageTitle: "Login",
+    });
+  },
+  signup: (req, res) => {
+    res.render("users/signup", {
+      pageTitle: "Registrarse",
+    });
+  },
+  profile: (req, res) => {
+    res.render("users/profile", {
+      pageTitle: "Perfil",
+      id: req.params.id,
+    });
+  },
+  edit: (req, res) => {
+    res.render("users/editUser", {
+      pageTitle: "Editar",
+    });
+  },
+  editPut: (req, res) => {
+    const resultValidation = validationResult(req);
+    if (resultValidation.errors.length > 0) {
+      return res.render("users/editUser", {
+        errors: resultValidation.mapped(),
+        pageTitle: "Editar",
+        oldData: req.body,
+      });
     }
-}
+  },
+};
 module.exports = usersController;
