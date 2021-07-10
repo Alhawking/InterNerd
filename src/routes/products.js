6fb5229815a36eeade39c07abc7475210ae8f5a0
francisco espinola <franespinola_92@hotmail.com>
2021-07-09 21:15:28 -0300

    agregue multer

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const multer=require('multer');
+const path=require('path')
+//configuracion disco almacenamiento multer//
+const multerDiskStorage = multer.diskStorage({
+    destination: function(req, file, cb) {       // request, archivo y callback que almacena archivo en destino
+     cb(null, path.join(__dirname,'../../public/images/products'));    //Ruta donde almacenamos el archivo
+    },
+    filename: function(req, file, cb) {          // request, archivo y callback que almacena archivo en destino
+     let imageName = 'img'+ '-' + file.originalname;    // milisegundos y extensión de archivo original
+     cb(null, imageName);         
+    }
+});
+const uploadFile=multer({storage:multerDiskStorage});
+
 const productsController = require('../controllers/productsController');
 router.get('/detail/:id?', productsController.detail);
 router.get('/create', productsController.create);
@@ -8,6 +22,6 @@ router.get('/category/:categoryName', productsController.category);
 router.get('/wires/:wireName', productsController.wires);
 router.get('/list', productsController.allProducts);
 //---POST--//
-router.post('/create',productsController.createPost)
+router.post('/create',uploadFile.single('imagenProducto'),productsController.createPost)
 
 module.exports = router;
